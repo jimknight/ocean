8895b0d90a8f4d3d334fba1a346b874bd6d09388
Jim Knight <jim.knight@lavatech.com>
2013-02-22 19:39:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ocean::Application.config.secret_token = 'c14e547b50d11fbe6d2df0204f3bfbfc4c2ca9b6ca572d36a402616945d9f160d6adf6c715101edf22ecee5ea7e3f2ab98359e0aca7f02da6735d19ebbd2950e'
